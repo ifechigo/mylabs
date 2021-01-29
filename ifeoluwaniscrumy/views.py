12e299b2f2384c45d012334054a735b8b7d04f89
Dada Ifeoluwani Adedotun <65918863+ifechigo@users.noreply.github.com>
2021-01-29 11:56:10 +0100

    changed index text

--- ifeoluwaniscrumy/views.py
+++ ifeoluwaniscrumy/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to Django")
+    return HttpResponse("This is a Scrum Application")
