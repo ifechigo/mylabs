a44bd775d5159375d0cea06e431fcad085f2ad3d
Dada Ifeoluwani Adedotun <65918863+ifechigo@users.noreply.github.com>
2021-02-05 16:26:36 +0100

    creating models to access admin

--- ifeoluwaniscrumy/admin.py
+++ ifeoluwaniscrumy/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+
+admin.site.register(ScrumyGoal)
+admin.site.register(ScrumyHistory)
+admin.site.register(GoalStatus)
+
--- ifeoluwaniscrumy/models.py
+++ ifeoluwaniscrumy/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import PROTECT
 
 # Create your models here.
+
+
+class ScrumyGoal(models.Model):
+    goal_status = models.ForeignKey(GoalStatus, on_delete=PROTECT)
+    user = models.ForeignKey(User, related_name="ifeoluwani", null=True, on_delete=models.SET_NULL)
+    goal_name = models.CharField(max=200)
+    goal_id = models.CharField(max=200)
+    created_by = models.CharField(max=200)
+    moved_by = models.CharField(max=200)
+    owner = models.CharField(max=200)
+
+    
+class ScrumyHistory(models.Model):
+    goal = models.ForeignKey(ScrumyGoal, null=True, on_delete=models.SET_NULL)
+    moved_by = models.CharField(max=200)
+    created_by = models.CharField(max=200)
+    moved_fro = models.CharField(max=200)
+    moved_to = models.CharField(max=200)
+    time_of_action = models.CharField(max=200)
+
+
+class GoalStatus(models.Model):
+    status_name = models.CharField(max=200)
