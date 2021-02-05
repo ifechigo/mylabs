17263e7b4625c865fb5724cda6e410a64d2cf098
Dada Ifeoluwani Adedotun <65918863+ifechigo@users.noreply.github.com>
2021-02-05 16:34:36 +0100

    creating models to access admin

--- ifeoluwaniscrumy/models.py
+++ ifeoluwaniscrumy/models.py
@@ -4,25 +4,26 @@ from django.db.models import PROTECT
 
 # Create your models here.
 
+ 
+class GoalStatus(models.Model):
+    status_name = models.CharField(max_length=200)
+
 
 class ScrumyGoal(models.Model):
     goal_status = models.ForeignKey(GoalStatus, on_delete=PROTECT)
     user = models.ForeignKey(User, related_name="ifeoluwani", null=True, on_delete=models.SET_NULL)
-    goal_name = models.CharField(max=200)
-    goal_id = models.CharField(max=200)
-    created_by = models.CharField(max=200)
-    moved_by = models.CharField(max=200)
-    owner = models.CharField(max=200)
+    goal_name = models.CharField(max_length=200)
+    goal_id = models.CharField(max_length=200)
+    created_by = models.CharField(max_length=200)
+    moved_by = models.CharField(max_length=200)
+    owner = models.CharField(max_length=200)
 
-    
+
 class ScrumyHistory(models.Model):
     goal = models.ForeignKey(ScrumyGoal, null=True, on_delete=models.SET_NULL)
-    moved_by = models.CharField(max=200)
-    created_by = models.CharField(max=200)
-    moved_fro = models.CharField(max=200)
-    moved_to = models.CharField(max=200)
-    time_of_action = models.CharField(max=200)
+    moved_by = models.CharField(max_length=200)
+    created_by = models.CharField(max_length=200)
+    moved_fro = models.CharField(max_length=200)
+    moved_to = models.CharField(max_length=200)
+    time_of_action = models.CharField(max_length=200)
 
-
-class GoalStatus(models.Model):
-    status_name = models.CharField(max=200)
