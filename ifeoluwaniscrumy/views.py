46443eab304c6bd3201192f79bb8f664049701bc
Dada Ifeoluwani Adedotun <65918863+ifechigo@users.noreply.github.com>
2021-02-18 14:20:13 +0100

    add add_goal and home view

--- ifeoluwaniscrumy/views.py
+++ ifeoluwaniscrumy/views.py
@@ -5,13 +5,6 @@ import random
 from .models import *
 
 # Create your views here.
-def generate_id():
-    random_num = random.randint(1000, 9999)
-    goal = ScrumyGoals.objects.filter(goal_id=random_num)
-
-    while goal:
-        continue
-    return random_num
 
 
 def index(request):
@@ -23,6 +16,7 @@ def move_goal(request, goal_id):
     goal = ScrumyGoals.objects.filter(goal_id=goal_id)
     return HttpResponse(goal)
 
+
 def add_goal(request):
     random_num = random.randint(1000, 9999)
 
@@ -30,10 +24,11 @@ def add_goal(request):
         print('goal_id exists')
 
     else:
-        first = ScrumyGoals.objects.create(goal_name = 'Keep learning Django', goal_id= random.randrange(1000,9999,2) , created_by= 'louis', moved_by='louis', owner= 'louis', goal_status= GoalStatus.objects.get(status_name = 'Weekly Goal'), user=User.objects.get(username = 'louis'))
+        first = ScrumyGoals.objects.create(goal_name = 'Keep learning Django', goal_id= random_num , created_by= 'louis', moved_by='louis', owner= 'louis', goal_status= GoalStatus.objects.get(status_name = 'Weekly Goal'), user=User.objects.get(username = 'louis'))
         first.save()
     
     return HttpResponse (first)
+
 
 def home(request):
     goals = ScrumyGoals.objects.all()
